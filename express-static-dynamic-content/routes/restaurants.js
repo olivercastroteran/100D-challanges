fa3820490a2c0f859ef4f28fd2f2afae2e548e9b
oliver <olivercastroteran@gmail.com>
2022-10-31 22:29:35 -0400

    routes implemented - code spliting

---
+++ express-static-dynamic-content/routes/restaurants.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const uuid = require('uuid');
+
+const resData = require('../util/restaurant-data');
+
+const router = express.Router();
+
+router.get('/confirm', function (req, res) {
+  res.render('confirm');
+});
+
+router.get('/recommend', function (req, res) {
+  res.render('recommend');
+});
+
+router.post('/recommend', function (req, res) {
+  const restaurant = req.body;
+  restaurant.id = uuid.v4();
+  const storedRestaurants = resData.getStoredRestaurants();
+
+  storedRestaurants.push(restaurant);
+  resData.storeRestaurants(storedRestaurants);
+
+  res.redirect('/confirm');
+});
+
+router.get('/restaurants', function (req, res) {
+  const storedRestaurants = resData.getStoredRestaurants();
+
+  res.render('restaurants', {
+    numOfRest: storedRestaurants.length,
+    restaurants: storedRestaurants,
+  });
+});
+
+router.get(`/restaurants/:id`, function (req, res) {
+  const restaurantId = req.params.id;
+  const storedRestaurants = resData.getStoredRestaurants();
+
+  for (const restaurant of storedRestaurants) {
+    if (restaurant.id === restaurantId) {
+      return res.render('restaurant-detail', { restaurant: restaurant });
+    }
+  }
+
+  res.status(404).render('404');
+});
+
+module.exports = router;
