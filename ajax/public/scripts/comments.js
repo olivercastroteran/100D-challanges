4af0e8d2ed7abe215657a45189cc1614e20801f8
oliver <olivercastroteran@gmail.com>
2022-11-25 00:32:22 -0400

    rendering comments with AJAX

--- ajax/public/scripts/comments.js
+++ ajax/public/scripts/comments.js
@@ -1,11 +1,29 @@
 const loadCommentsBtn = document.getElementById('load-comments-btn');
+const commentsSection = document.getElementById('comments');
+
+function createCommentsList(comments) {
+  const commentsList = document.createElement('ol');
+  for (const comment of comments) {
+    const commentEl = document.createElement('li');
+    commentEl.innerHTML = `
+      <article class="comment-item">
+        <h2>${comment.title}</h2>
+        <p>${comment.text}</p>
+      </article>
+    `;
+    commentsList.appendChild(commentEl);
+  }
+  return commentsList;
+}
 
 async function fetchComments(e) {
   const postId = loadCommentsBtn.dataset.postid;
-  console.log(loadCommentsBtn.data);
   const response = await fetch(`/posts/${postId}/comments`);
   const data = await response.json();
-  console.log(data);
+
+  const commentsList = createCommentsList(data);
+  commentsSection.innerHTML = '';
+  commentsSection.appendChild(commentsList);
 }
 
 loadCommentsBtn.addEventListener('click', fetchComments);
